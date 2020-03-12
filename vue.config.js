a4cb019bf8a007b6dcfa8122b0e3848530a4a56f
zzl <784778060@qq.com>
2020-03-12 10:12:27 +0800

    更改publicPath测试github部署

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/bar/" : "/",
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: false,//process.env.NODE_ENV === 'development'
